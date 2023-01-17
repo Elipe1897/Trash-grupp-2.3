5d9d3e7746eca0967972e6e6fb09f9f24e89a4d1
Elipe1897 <elias.pettersson@elev.ga.lbs.se>
2023-01-17 14:19:55 +0100

    BANNANANA WORK

--- Assets/Scripts Elias/ScrapDestroy.cs
+++ Assets/Scripts Elias/ScrapDestroy.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class ScrapDestroy : MonoBehaviour
 {
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -16,14 +17,20 @@ public class ScrapDestroy : MonoBehaviour
         
     }
 
-
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.transform.tag == "Player")
+        if (collision.transform.tag == "Ground")
+
         {
-          Destroy(gameObject);
+            rb.velocity = Vector3.zero;
+            rb.isKinematic = true;
+        }
+        if (collision.transform.tag == "Player")
+        {
+            Destroy(gameObject);
             ScoreController.instance.AddSCrap(1);
         }
-       
     }
+
 }
