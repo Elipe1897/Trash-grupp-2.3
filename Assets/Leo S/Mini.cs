37ce8d5bc62267700989ab029b5a5b58d6328061
onnud <leo.soderberg@elev.ga.lbs.se>
2023-01-17 09:59:19 +0100

    lights, enemy skit

---
+++ Assets/Leo S/Mini.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mini : EnemyAI
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 5;
+        launchForce = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Leo S/TrackingProjectile.cs
+++ Assets/Leo S/TrackingProjectile.cs
@@ -3,16 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using Pathfinding;
 
-public class EnemyAI : MonoBehaviour
+public class TrackingProjectile : MonoBehaviour
 {
     [Header("Movement")]
     public Transform target;
-    public Transform enemyGFX;
 
-    float maxTargetDistance;
     public float nextWaypointDistance = 3f;
 
-    public bool jump;
     public float speed;
 
     Path path;
@@ -20,11 +17,6 @@ public class EnemyAI : MonoBehaviour
     int currentWaypoint;
     bool reachedWaypoint;
 
-    [Header("Attacking")]
-    public GameObject projectile;
-    public Transform attackPoint;
-    public int launchForce;
-    float attackTimer;
 
     Seeker seeker;
     Rigidbody2D rb;
@@ -35,18 +27,17 @@ public class EnemyAI : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
 
         InvokeRepeating("UpdatePath", 0f, .5f);
-        maxTargetDistance = 7.5f;
     }
 
     void UpdatePath()
     {
-        if(seeker.IsDone())
+        if (seeker.IsDone())
             seeker.StartPath(rb.position, target.position, OnPathComplete);
     }
 
     void OnPathComplete(Path p)
     {
-        if(!p.error)
+        if (!p.error)
         {
             path = p;
             currentWaypoint = 0;
@@ -74,64 +65,23 @@ public class EnemyAI : MonoBehaviour
         float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
         float targetDistance = Vector2.Distance(rb.position, target.position);
 
-        if(targetDistance <= maxTargetDistance)
-        {
-            rb.AddForce(force);
-            maxTargetDistance = 14;
-        }
-        else if(targetDistance >= maxTargetDistance)
-        {
-            maxTargetDistance = 7;
-        }
+         rb.AddForce(force); 
 
-        if(distance < nextWaypointDistance)
+        if (distance < nextWaypointDistance)
         {
             currentWaypoint++;
         }
 
-        if(force.x >= -0.01f)
+        if (force.x >= -0.01f)
         {
             transform.localScale = new Vector3(-1, 1, 1);
         }
-        else if(force.x <= 0.01f)
+        else if (force.x <= 0.01f)
         {
             transform.localScale = new Vector3(1, 1, 1);
         }
 
-        if(jump == true)
-        {
-            rb.AddForce(new Vector2(0, 5));
-        }
-
-        //Attacking
-
-        attackTimer += Time.deltaTime;
-        if(attackTimer > 3)
-        {
-            Attack();
-            attackTimer = 0;
-        }
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.transform.tag == "Obstacle")
-        {
-            jump = true;
-        }
-        
-    }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.transform.tag == "Obstacle")
-        {
-            jump = false;
-        }
     }
 
-    void Attack()
-    {
-        GameObject newProjectile = Instantiate(projectile, attackPoint.position, attackPoint.rotation);
-        newProjectile.GetComponent<Rigidbody2D>().velocity = transform.right * launchForce;
-    }
 
 }
