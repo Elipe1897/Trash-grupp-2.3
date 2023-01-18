851221af1953b1d6b7032f9734096a40cc8a8a60
DarreTheKing <97673555+DarreTheKing@users.noreply.github.com>
2023-01-18 10:48:16 +0100

    trash spawner i level one

---
+++ Assets/Scripts Darian/Spawner.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject Trash;
+
+    float maxSpawnRateInSeconds = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("SpawnTrash", maxSpawnRateInSeconds);
+
+        InvokeRepeating("IncreaseSpawnRate", 0f, 30f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void SpawnTrash()
+    {
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+
+        GameObject anTrash = (GameObject)Instantiate(Trash);
+        anTrash.transform.position = new Vector2(Random.Range(min.x, max.x), max.y);
+
+        ScheduleNextTrashSpawn();
+    }
+    void ScheduleNextTrashSpawn()
+    {
+        float spawnInNSeconds;
+        if (maxSpawnRateInSeconds > 2f)
+        {
+            spawnInNSeconds = Random.Range(2f, maxSpawnRateInSeconds);
+        }
+        else
+            spawnInNSeconds = 1f;
+        Invoke("SpawnTrash", spawnInNSeconds);
+    }
+    void IncreaseSpawnRate()
+    {
+        if (maxSpawnRateInSeconds > 2f)
+            maxSpawnRateInSeconds--;
+
+        if (maxSpawnRateInSeconds == 2f)
+            CancelInvoke("IncreaseSpawnRate");
+    }
+}
