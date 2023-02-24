83f134447c8569aea539b602e864ef1b5f186018
onnud <leo.soderberg@elev.ga.lbs.se>
2023-02-24 11:20:33 +0100

    commemt

--- Assets/Leo S/Scripts/Boss.cs
+++ Assets/Leo S/Scripts/Boss.cs
@@ -3,22 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+// Leo S
 public class Boss : MonoBehaviour
 {
-    [SerializeField] private AudioSource bossSoundEffect;
-    public Transform target;
+    [SerializeField] private AudioSource bossSoundEffect; // varaible for the audiosource
+    public Transform target; // variable for the player 
 
-    public GameObject [] scrap;
-    public Transform attackPoint;
-    public int launchForce;
-    float attackTimer;
+    public GameObject [] scrap; // variable for the scrap projectile
+    public Transform attackPoint; // a variable for where the scrap projectile will spawn
+    public int launchForce; // how fast the scrap will be shot
+    float attackTimer; // when the boss will attack
+    public float attackDistance; // how far distance it has to be for the boss to attack
 
+    //variables for the states
     private enum State { idle, hurt,attack, death};
     private State state = State.idle;
 
-    public int health;
+    public int health; // a varaible for the boss health
 
-    public Animator anim;
+    public Animator anim; // a varialble for the animator
 
     // Start is called before the first frame update
     void Start()
@@ -29,8 +32,11 @@ public class Boss : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float targetDistance = Vector2.Distance(transform.position, target.position);
-        if (targetDistance < 10)
+        float targetDistance = Vector2.Distance(transform.position, target.position); // Calculates the distance between the Boss and the player
+
+        //If you are close enough to the player it starts an attack timer
+        //if the attack timer reaches 3 it loads the attack coroutine and resets the timer
+        if (targetDistance < attackDistance)
         {
             attackTimer += Time.deltaTime;
             if (attackTimer > 3)
@@ -40,19 +46,21 @@ public class Boss : MonoBehaviour
             }
         }
        
-        StateSwitch();
-        anim.SetInteger("state", (int)state);
+        StateSwitch(); // Loads the StateSwitch function
+        anim.SetInteger("state", (int)state); // Animates the boss depending on what state the boss is in
 
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // If you collide with a bullet you load the take damage function
         if (collision.transform.tag == "Bullet")
         {
             TakeDamage();
         }
 
     }
+    //Deals 1 damage and changes the bosses state to hurt
     void TakeDamage()
     {
         state = State.hurt;
@@ -62,30 +70,35 @@ public class Boss : MonoBehaviour
     void Attack()
     {
         int random = Random.Range(0, 2);
-        Vector2 myPos = new Vector2(attackPoint.position.x, attackPoint.position.y); //our curr position is where our muzzle points
+        Vector2 myPos = new Vector2(attackPoint.position.x, attackPoint.position.y); //our current position is where our attack point is
         GameObject projectile = Instantiate(scrap[random], myPos, Quaternion.identity); //create our bullet
         Vector2 direction = myPos - (Vector2)target.position; //get the direction to the target
-        projectile.GetComponent<Rigidbody2D>().velocity = -direction * launchForce;
+        projectile.GetComponent<Rigidbody2D>().velocity = -direction * launchForce; // Add a force to teh projectile
     }
+
+    //Changes states to animate the boss
     public void StateSwitch()
     {
-        if (health <= 0)
+        if (health <= 0) // if health is lesser than 1 the state changes to death and the Die coroutine loads
         {
-            health = 0;
             state = State.death;
             StartCoroutine(Die());
         }
         else
         {
-            state = State.idle;
+            state = State.idle; // if the boss doesnt do anything the bosses state is idle
         }
     }
     public IEnumerator Attacks()
     {
+        //Changes the bosses state to the attacking state
         state = State.attack;
+
         yield return new WaitForSeconds(0.4f);
+        //Loads the Attack function after .4 seconds
         Attack();
     }
+    // Destroys the boss and loads the victoryscene
     public IEnumerator Die()
     {
         yield return new WaitForSeconds(.45f);
